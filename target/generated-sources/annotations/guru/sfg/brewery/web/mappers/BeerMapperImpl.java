fed359f332156e05f0196fe0bb2b7f32df05ad9b
thrwwy216 <thrwwy216@tutanota.com>
2020-12-27 03:12:32 -0600

    init commit

--- target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-12-27T02:19:03-0600",
+    date = "2020-12-27T03:11:19-0600",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 13.0.2 (Oracle Corporation)"
 )
 @Component
